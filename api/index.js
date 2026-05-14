239df0c69e9d3340513be6f971d4105a2f06b5c1
Mankowar <147993515+Mankowar@users.noreply.github.com>
2026-05-14 17:28:21 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,7 @@
-export const config = {
- runtime: 'edge',
- regions: ['iad1'], 
-};
-
+export const config = { runtime: 'edge', regions: ['iad1'] };
 export default async function handler(req) {
  const url = new URL(req.url);
- const targetUrl = new URL(url.pathname + url.search, 'https://generativelanguage.googleapis.com');
- const response = await fetch(targetUrl, {
-  method: req.method,
-  headers: { 'Content-Type': 'application/json' },
-  body: req.body
- });
+ const targetUrl = new URL('/v1beta/models/gemini-2.0-flash:generateContent' + url.search, 'https://generativelanguage.googleapis.com');
+ const response = await fetch(targetUrl, { method: req.method, headers: { 'Content-Type': 'application/json' }, body: req.body });
  return new Response(response.body, { status: response.status, headers: response.headers });
 }
