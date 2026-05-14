bb3016ba7d705c77c15d0a5d2da4fbe4d48f527e
Mankowar <147993515+Mankowar@users.noreply.github.com>
2026-05-14 16:33:11 +0900

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,15 @@
+export const config = {
+ runtime: 'edge',
+ regions: ['iad1'], 
+};
+
+export default async function handler(req) {
+ const url = new URL(req.url);
+ const targetUrl = new URL(url.pathname + url.search, 'https://generativelanguage.googleapis.com');
+ const response = await fetch(targetUrl, {
+  method: req.method,
+  headers: { 'Content-Type': 'application/json' },
+  body: req.body
+ });
+ return new Response(response.body, { status: response.status, headers: response.headers });
+}
